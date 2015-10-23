f467dcc2c94084e699dbd2b7655d1624b6053588
Robin Orheden <orhedenr@gmail.com>
2015-10-23 13:56:16 +0200

    add support for environment variable alias

--- test/strategy/LoadEnvConfigStrategyTest.js
+++ test/strategy/LoadEnvConfigStrategyTest.js
@@ -7,38 +7,72 @@ var strategy = require('../../lib/strategy');
 var LoadEnvConfigStrategy = strategy.LoadEnvConfigStrategy;
 
 describe('LoadEnvConfigStrategy', function () {
-  var testPrefix = "TEST";
+  var testPrefix, testConfig, testVariables;
 
-  var testConfig = {
-    that: {
-      exist: null,
-      exists: {
-        and: {
-          areVery: {
-            deep: null
+  beforeEach(function () {
+    testPrefix = "TEST";
+
+    testConfig = {
+      that: {
+        exist: null,
+        is: {
+          alias: null
+        },
+        exists: {
+          and: {
+            areVery: {
+              deep: null
+            }
           }
         }
       }
-    }
-  };
+    };
 
-  var testVariables = {
-    TEST_THAT_EXIST: "123",
-    TEST_THAT_EXISTS_AND_AREVERY_DEEP: "456",
-    TEST_THAT_DOESNT_EXIST: "789",
-    THAT_EXIST: "zzz"
-  };
+    testVariables = {
+      TEST_THAT_EXIST: "123",
+      TEST_THAT_EXISTS_AND_AREVERY_DEEP: "456",
+      TEST_THAT_SHOULDNT_EXIST: "789",
+      TEST_THAT_HAS_ALIAS: 'abc',
+      THAT_EXIST: "zzz"
+    };
 
-  before(function () {
     for (var key in testVariables) {
       process.env[key] = testVariables[key];
     }
   });
 
-  after(function () {
+  afterEach(function () {
     for (var key in testVariables) {
       delete process.env[key];
     }
+  });
+
+  it('should load environment variables with respect to alias', function (done) {
+    var strategy = new LoadEnvConfigStrategy(testPrefix, {
+      TEST_THAT_IS_ALIAS: 'TEST_THAT_HAS_ALIAS'
+    });
+
+    strategy.process(testConfig, function (err, config) {
+      assert.isNull(err);
+
+      assert.deepEqual(config, {
+        that: {
+          exist: "123",
+          is: {
+            alias: "abc"
+          },
+          exists: {
+            and: {
+              areVery: {
+                deep: "456"
+              }
+            }
+          }
+        }
+      });
+
+      done();
+    });
   });
 
   it("should load environment variables that map to existing properties on config", function (done) {
@@ -49,6 +83,9 @@ describe('LoadEnvConfigStrategy', function () {
       assert.deepEqual(config, {
         that: {
           exist: "123",
+          is: {
+            alias: null
+          },
           exists: {
             and: {
               areVery: {

d48bdd2e511f8b600e4fda4c516f29163d5fca31
Robin Orheden <orhedenr@gmail.com>
2016-03-02 10:47:13 -0800

    fix fieldOrder array extend errors for web.login and web.register + tests

--- test/strategy/ExtendConfigStrategyTest.js
+++ test/strategy/ExtendConfigStrategyTest.js
@@ -83,28 +83,109 @@ describe('ExtendConfigStrategy', function () {
     });
   });
 
-  it('should preserve prototype references', function() {
-    var baseConfig = {
-      foo: 'bar',
-      cacheOptions: { }
-    };
+  describe('extension hacks', function () {
+    it('should preserve the cacheOptions.client object reference', function() {
+      var baseConfig = {
+        foo: 'bar',
+        cacheOptions: { }
+      };
 
-    function MockCacheClient(){
-      return this;
-    }
-    MockCacheClient.prototype.get = function get() { };
-    MockCacheClient.prototype.set = function set() { };
-    var extendConfig = {
-      cacheOptions: {
-        client: new MockCacheClient()
+      function MockCacheClient(){
+        return this;
       }
-    };
-    var strategy = new ExtendConfigStrategy(extendConfig);
+      MockCacheClient.prototype.get = function get() { };
+      MockCacheClient.prototype.set = function set() { };
+      var extendConfig = {
+        cacheOptions: {
+          client: new MockCacheClient()
+        }
+      };
+      var strategy = new ExtendConfigStrategy(extendConfig);
 
-    strategy.process(baseConfig, function(err, newConfig){
-      assert.isNull(err);
-      assert.equal(newConfig.foo, baseConfig.foo);
-      assert.equal(newConfig.cacheOptions.client.get, extendConfig.cacheOptions.client.get);
+      strategy.process(baseConfig, function(err, newConfig){
+        assert.isNull(err);
+        assert.equal(newConfig.foo, baseConfig.foo);
+        assert.equal(newConfig.cacheOptions.client.get, extendConfig.cacheOptions.client.get);
+      });
+    });
+
+    it('should overwrite the web.produces array', function () {
+      var baseConfig = {
+        web: {
+          produces: ['df9d24dd-59b2-4485-a1ae-18b3b04e71fd', 'fa845bd0-541c-4062-b52c-6757b372d8e2', '414c77f8-45d6-413d-aa49-122ddb62c82c']
+        }
+      };
+
+      var extendConfig = {
+        web: {
+          produces: ['81b52001-384f-4944-adc8-56cc81c6fa62']
+        }
+      };
+
+      var strategy = new ExtendConfigStrategy(extendConfig);
+
+      strategy.process(baseConfig, function(err, newConfig){
+        assert.isNull(err);
+        assert.deepEqual(newConfig.web.produces, extendConfig.web.produces);
+      });
+    });
+
+    it('should overwrite the web.login.form.fieldOrder array', function () {
+      var baseConfig = {
+        web: {
+          login: {
+            form: {
+              fieldOrder: ['53b83ca6-b34c-41ab-95da-5d6df830b411', '9488945a-bb61-4ee4-92ab-810f0238f67e', 'c1e11faf-0928-4cec-8b84-48af0ff006a1']
+            }
+          }
+        }
+      };
+
+      var extendConfig = {
+        web: {
+          login: {
+            form: {
+              fieldOrder: ['f066405c-4232-41d1-a63d-e99a11b3e894']
+            }
+          }
+        }
+      };
+
+      var strategy = new ExtendConfigStrategy(extendConfig);
+
+      strategy.process(baseConfig, function(err, newConfig){
+        assert.isNull(err);
+        assert.deepEqual(newConfig.web.login.form.fieldOrder, extendConfig.web.login.form.fieldOrder);
+      });
+    });
+
+    it('should overwrite the web.register.form.fieldOrder array', function () {
+      var baseConfig = {
+        web: {
+          register: {
+            form: {
+              fieldOrder: ['cabacd56-d996-49e1-bd3c-2fbac565473d', 'ccfb9bed-f2e6-4ca8-a854-ce0bae55c941', '7106bfd1-df64-40c1-b379-cd18dde9cb43']
+            }
+          }
+        }
+      };
+
+      var extendConfig = {
+        web: {
+          register: {
+            form: {
+              fieldOrder: ['7d360fe6-0708-470a-857e-46170c84dbe0']
+            }
+          }
+        }
+      };
+
+      var strategy = new ExtendConfigStrategy(extendConfig);
+
+      strategy.process(baseConfig, function(err, newConfig){
+        assert.isNull(err);
+        assert.deepEqual(newConfig.web.register.form.fieldOrder, extendConfig.web.register.form.fieldOrder);
+      });
     });
   });
 });
